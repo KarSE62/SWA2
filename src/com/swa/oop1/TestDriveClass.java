edf3168c613eeb95ad8fd5e9c055d862f63fe0d1
KarSE62 <59428783+KarSE62@users.noreply.github.com>
2021-07-12 15:47:33 +0700

    rename variable and method

--- src/com/swa/oop1/TestDriveClass.java
+++ src/com/swa/oop1/TestDriveClass.java
@@ -1,11 +1,8 @@
 package com.swa.oop1;
 
 public class TestDriveClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Product p = new Product("P0001","Nintendo Switch");
-        System.out.println(p.getDetailProduct());
-	}
-
+    public static void main(String[] args) {
+        Product p = new Product("P0001","Nintendo Switch");
+        System.out.println(p.getDetail());
+    }
 }
